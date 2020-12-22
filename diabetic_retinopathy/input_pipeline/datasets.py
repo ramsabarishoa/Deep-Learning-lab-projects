2f7be2cee5650881a0827b32f480a57c8944e390
st169481 <st169481@stud.uni-stuttgart.de>
2020-12-22 10:00:13 +0100

    Update datasets.py

--- diabetic_retinopathy/input_pipeline/datasets.py
+++ diabetic_retinopathy/input_pipeline/datasets.py
@@ -1,81 +1,120 @@
-import gin
 import logging
-import tensorflow as tf
-import tensorflow_datasets as tfds
+import pandas as pd
+import zipfile
+import glob, os
+import sys
+import matplotlib.pyplot as plt
 
-from input_pipeline.preprocessing import preprocess, augment
+'''pd.set_option('display.max_rows', 600)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 600)
 
-@gin.configurable
-def load(name, data_dir):
-    if name == "idrid":
-        logging.info(f"Preparing dataset {name}...")
-        # ...
+np.set_printoptions(threshold=1000)'''
 
-        return
+class data_preparation:
+    """Class containing functions for data loading """
 
-    elif name == "eyepacs":
-        logging.info(f"Preparing dataset {name}...")
-        (ds_train, ds_val, ds_test), ds_info = tfds.load(
-            'diabetic_retinopathy_detection/btgraham-300',
-            split=['train', 'validation', 'test'],
-            shuffle_files=True,
-            with_info=True,
-            data_dir=data_dir
-        )
+    def load(self, data_dir, DEBUG_INPUT_PIPELINE):
+        """ Function to check the dataset path and load the ground truths.
+        Classifies the labels into 0's and 1's
+        Plots the labels in Debug mode."""
 
-        def _preprocess(img_label_dict):
-            return img_label_dict['image'], img_label_dict['label']
+        self.train_images = glob.glob(data_dir + "/images/train/*.jpg")
+        print('Total number of training images:', len(self.train_images))
 
-        ds_train = ds_train.map(_preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        ds_val = ds_val.map(_preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        ds_test = ds_test.map(_preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        self.df_train = pd.read_csv(data_dir + '/labels/train.csv')
+        self.df_train = self.df_train.drop_duplicates()
+        self.df_train = self.df_train.iloc[:, : 2]
 
-        return prepare(ds_train, ds_val, ds_test, ds_info)
+        if DEBUG_INPUT_PIPELINE == 1:
+            print(self.df_train.head())
+            self.df_train[['Retinopathy grade']].hist(figsize=(10, 5))
+            plt.title('Groundtruth Labels for Diabetic Retinopathy before binarization')
+            plt.xlabel('Label')
+            plt.ylabel('Number of Images')
+            plt.show()
 
-    elif name == "mnist":
-        logging.info(f"Preparing dataset {name}...")
-        (ds_train, ds_val, ds_test), ds_info = tfds.load(
-            'mnist',
-            split=['train[:90%]', 'train[90%:]', 'test'],
-            shuffle_files=True,
-            as_supervised=True,
-            with_info=True,
-            data_dir=data_dir
-        )
+        """Classifying the labels [0,1] to 0 and [2,3,4] to 1
+        NRDR is considered as having no or mild non-proliferative DR (labels 0 and 1). 
+        RDR is considered as having moderate, severe, or proliferative DR (labels 2 and up)."""
 
-        return prepare(ds_train, ds_val, ds_test, ds_info)
+        self.df_train['Retinopathy grade'] = self.df_train['Retinopathy grade'].replace([0, 1, 2, 3, 4], [0, 0, 1, 1, 1])
+        if DEBUG_INPUT_PIPELINE == 1:
+            print(self.df_train.head())
+            self.df_train[['Retinopathy grade']].hist(figsize=(10, 5))
+            self.df_train = self.df_train.sample(frac=1).reset_index(drop=True)
+            plt.title('Groundtruth Labels for Diabetic Retinopathy after binarization')
+            plt.xlabel('Label')
+            plt.ylabel('Number of Images')
+            plt.show()
 
-    else:
-        raise ValueError
 
-@gin.configurable
-def prepare(ds_train, ds_val, ds_test, ds_info, batch_size, caching):
-    # Prepare training dataset
-    ds_train = ds_train.map(
-        preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    if caching:
-        ds_train = ds_train.cache()
-    ds_train = ds_train.map(
-        augment, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples // 10)
-    ds_train = ds_train.batch(batch_size)
-    ds_train = ds_train.repeat(-1)
-    ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+    def balancing_dataset(self, DEBUG_INPUT_PIPELINE):
+        """ Function to balance the dataset by over-sampling the minority class """
 
-    # Prepare validation dataset
-    ds_val = ds_val.map(
-        preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds_val = ds_val.batch(batch_size)
-    if caching:
-        ds_val = ds_val.cache()
-    ds_val = ds_val.prefetch(tf.data.experimental.AUTOTUNE)
+        N_Training = round(len(self.df_train) * 0.8)
+        train = self.df_train[:N_Training]
+        self.validation = self.df_train[N_Training:]
+        print('---------------------------------------------------------')
+        print('Splitting the train samples into train and validation set')
+        print('---------------------------------------------------------')
+        print('Number of training samples:', len(train))
+        print('Number of validation samples:', len(self.validation))
 
-    # Prepare test dataset
-    ds_test = ds_test.map(
-        preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds_test = ds_test.batch(batch_size)
-    if caching:
-        ds_test = ds_test.cache()
-    ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+        label_0 = train[train['Retinopathy grade'] == 0]
+        label_1 = train[train['Retinopathy grade'] == 1]
+        print('---------------------------------------------------------')
+        print('Number of images with Label 0 before sampling:', len(label_0))
+        print('Number of images with Label 1 before sampling:', len(label_1))
+        print('---------------------------------------------------------')
 
-    return ds_train, ds_val, ds_test, ds_info
+        label_count_1, label_count_0 = train['Retinopathy grade'].value_counts()
+        label_0 = label_0.sample(label_count_1, replace=True)
+        self.df_train_sampled = pd.concat([label_0, label_1])
+        self.df_train_sampled = self.df_train_sampled.sample(frac=1, random_state=0)
+        print('Number of images with Label 0 after over-sampling:', len(label_0))
+        print('Number of images with Label 1 after over-sampling:', len(label_1))
+        print('---------------------------------------------------------')
+
+        if DEBUG_INPUT_PIPELINE == 1:
+            print(self.df_train_sampled)
+            self.df_train_sampled[['Retinopathy grade']].hist(figsize = (10, 5))
+            plt.show()
+
+    def to_preprocessing(self):
+        """Function to send the data for further preprocessing."""
+        train_images_list = []
+        train_labels_list = []
+        val_images_list = []
+        val_labels_list = []
+
+        self.df_train_sampled.loc[:,'Image name'] = self.df_train_sampled.loc[:,'Image name'] + '.jpg'
+        self.validation.loc[:,'Image name'] = self.validation.loc[:,'Image name'] + '.jpg'
+
+        for tname, tclass in self.df_train_sampled.itertuples(index=False):
+            for ft in self.train_images:
+                if os.path.basename(ft) == tname:
+                    # print(fp,iname,iclass)
+                    train_images_list.append(ft)
+                    train_labels_list.append(tclass)
+
+        for vname, vclass in self.validation.itertuples(index=False):
+            for fv in self.train_images:
+                if os.path.basename(fv) == vname:
+                    # print(fv,vname,vclass)
+                    val_images_list.append(fv)
+                    val_labels_list.append(vclass)
+
+        print(len(train_images_list), len(train_labels_list), len(val_images_list), len(val_labels_list))
+        return train_images_list, train_labels_list, val_images_list, val_labels_list
+
+
+""" Class Instantiation"""
+load = data_preparation()
+dataset_path = input("Enter the path for the dataset and unzip the contents: ")
+Debug = '''The Debug option here helps in analysing the histograms of the dataframes(train and test).
+It also displays the format of the data in the .csv files'''
+print(Debug)
+debug_mode_input_pipeline = int(input('Enter 1 for enabling debug option(DEBUG_INPUT_PIPELINE) else enter 0 : '))
+load.load(dataset_path,debug_mode_input_pipeline)
+load.balancing_dataset(debug_mode_input_pipeline)
