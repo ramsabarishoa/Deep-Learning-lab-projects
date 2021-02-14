a71d96c92c4ddb55f95c1acee54e422a85d7d2b7
st169481 <st169481@stud.uni-stuttgart.de>
2021-02-14 10:57:25 +0100

    Update train.py

--- diabetic_retinopathy/train.py
+++ diabetic_retinopathy/train.py
@@ -1,97 +1,43 @@
-import gin
+'''The model is trained and saved according to the number of epochs.
+Also the model checkpoints are saved are running each epoch.
+Please adapt the path to save the model accordingly'''
+
 import tensorflow as tf
-import logging
+import os
+from input_pipeline.preprocessing import train_generator, val_generator
+from models.layers import mdl
 
-@gin.configurable
-class Trainer(object):
-    def __init__(self, model, ds_train, ds_val, ds_info, run_paths, total_steps, log_interval, ckpt_interval):
-        # Summary Writer
-        # ....
+#Path to save the model and checkpoints
+path_to_save_model = input("Enter the path for saving the model and checkpoints: ")
 
-        # Checkpoint Manager
-        # ...
+checkpoint_path = path_to_save_model + '/' + 'cp.ckpt'
+checkpoint_dir = os.path.dirname(checkpoint_path)
 
-        # Loss objective
-        self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        self.optimizer = tf.keras.optimizers.Adam()
+if not os.path.exists(path_to_save_model):
+    pass
+else:
+    # Create a callback that saves the model's weights
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+def Trainer(epochs):
+    """Function to train the compiled model based on the dataset inputs and number of epochs"""
+    epochs = epochs
+    history = mdl.fit(
+        train_generator,
+        validation_data=val_generator,
+        epochs=epochs,
+        callbacks=[cp_callback]        
+    )
 
-        # Metrics
-        self.train_loss = tf.keras.metrics.Mean(name='train_loss')
-        self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+    if not os.path.exists(path_to_save_model):
+        pass
+    else:
+        mdl.save(path_to_save_model + '/' + 'DRD_Model.h5')
 
-        self.test_loss = tf.keras.metrics.Mean(name='test_loss')
-        self.test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+    return history
+print(''' ***************************Start Training************************''')
+epochs = 150
 
-        self.model = model
-        self.ds_train = ds_train
-        self.ds_val = ds_val
-        self.ds_info = ds_info
-        self.run_paths = run_paths
-        self.total_steps = total_steps
-        self.log_interval = log_interval
-        self.ckpt_interval = ckpt_interval
-
-    @tf.function
-    def train_step(self, images, labels):
-        with tf.GradientTape() as tape:
-            # training=True is only needed if there are layers with different
-            # behavior during training versus inference (e.g. Dropout).
-            predictions = self.model(images, training=True)
-            loss = self.loss_object(labels, predictions)
-        gradients = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
-
-        self.train_loss(loss)
-        self.train_accuracy(labels, predictions)
-
-    @tf.function
-    def test_step(self, images, labels):
-        # training=False is only needed if there are layers with different
-        # behavior during training versus inference (e.g. Dropout).
-        predictions = self.model(images, training=False)
-        t_loss = self.loss_object(labels, predictions)
-
-        self.test_loss(t_loss)
-        self.test_accuracy(labels, predictions)
-
-    def train(self):
-        for idx, (images, labels) in enumerate(self.ds_train):
-
-            step = idx + 1
-            self.train_step(images, labels)
-
-            if step % self.log_interval == 0:
-
-                # Reset test metrics
-                self.test_loss.reset_states()
-                self.test_accuracy.reset_states()
-
-                for test_images, test_labels in self.ds_val:
-                    self.test_step(test_images, test_labels)
-
-                template = 'Step {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
-                logging.info(template.format(step,
-                                             self.train_loss.result(),
-                                             self.train_accuracy.result() * 100,
-                                             self.test_loss.result(),
-                                             self.test_accuracy.result() * 100))
-
-                # Reset train metrics
-                self.train_loss.reset_states()
-                self.train_accuracy.reset_states()
-
-                # Write summary to tensorboard
-                # ...
-
-                yield self.test_accuracy.result().numpy()
-
-            if step % self.ckpt_interval == 0:
-                logging.info(f'Saving checkpoint to {self.run_paths["path_ckpts_train"]}.')
-                # Save checkpoint
-                # ...
-
-            if step % self.total_steps == 0:
-                logging.info(f'Finished training after {step} steps.')
-                # Save final checkpoint
-                # ...
-                return self.test_accuracy.result().numpy()
+history = Trainer(epochs)
+print(''' ***************************End Training************************''')
