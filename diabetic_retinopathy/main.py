59a37451369419a8b9ad19a1d8367585f28817bb
st169481 <st169481@stud.uni-stuttgart.de>
2021-02-14 12:20:30 +0100

    Update main.py

--- diabetic_retinopathy/main.py
+++ diabetic_retinopathy/main.py
@@ -1,44 +1,60 @@
-import gin
 import logging
 from absl import app, flags
-
-from train import Trainer
-from evaluation.eval import evaluate
-from input_pipeline import datasets
-from utils import utils_params, utils_misc
-from models.architectures import vgg_like
-
-FLAGS = flags.FLAGS
-flags.DEFINE_boolean('train', True, 'Specify whether to train or evaluate a model.')
+import os
+Project_Header = '''|--------------------------------------------------------------------------------------------|
+|Diabetic Retinopathy Detection Project - Team05                                             |
+|--------------------------------------------------------------------------------------------|
+|Team Members :                                                                              |
+|1.  Ram Sabarish Obla Amar Bapu     |st169693|  email ID:  st169693@stud.uni-stuttgart.de   |
+|2.  Swetha Lakshmana Murthy         |st169481|  email ID:  st169481@stud.uni-stuttgart.de   |
+|--------------------------------------------------------------------------------------------|
+The dataset used here is IDRID dataset. This contains a total of 516 fundus images of which 413 belongs to training and the remaining 103 belongs to the test dataset.
+The entire project is divided into sequence of events. Please select the necessary ones to view their corresponding results.
+Please install the necessary packages.
+Folder structure of the dataset
+    ROOT_FOLDER(/home/user/.../IDRID_dataset)
+       |-------- images
+       |            |------ train
+       |            |           |------ IDRiD_001.jpg
+       |            |                   etc...
+       |            |------ test
+       |                        |------ IDRiD_001.jpg
+       |                                etc...
+       |
+       | -------- labels                 
+       |             |
+       |             | ----- test.csv
+       |             | ----- train.csv
+       |
+    ```
+Creation of an input pipeline for the IDRID dataset
+Changes to be made according to your requirements is mentioned with '**TO_BE_CHANGED**' macro.
+Please adapt it accordingly.
+Folder structure of the dataset
+    ROOT_FOLDER(/home/user/.../IDRID_dataset)
+       |-------- images
+       |            |------ train
+       |            |           |------ IDRiD_001.jpg
+       |            |                   etc...
+       |            |------ test
+       |                        |------ IDRiD_001.jpg
+       |                                etc...
+       |
+       | -------- labels                 
+       |             |
+       |             | ----- test.csv
+       |             | ----- train.csv
+       |
+    ```
+'''
 
 def main(argv):
-
-    # generate folder structures
-    run_paths = utils_params.gen_run_folder()
-
-    # set loggers
-    utils_misc.set_loggers(run_paths['path_logs_train'], logging.INFO)
-
-    # gin-config
-    gin.parse_config_files_and_bindings(['configs/config.gin'], [])
-    utils_params.save_config(run_paths['path_gin'], gin.config_str())
-
-    # setup pipeline
-    ds_train, ds_val, ds_test, ds_info = datasets.load()
-
-    # model
-    model = vgg_like(input_shape=ds_info.features["image"].shape, n_classes=ds_info.features["label"].num_classes)
-
-    if FLAGS.train:
-        trainer = Trainer(model, ds_train, ds_val, ds_info, run_paths)
-        for _ in trainer.train():
-            continue
-    else:
-        evaluate(model,
-                 checkpoint,
-                 ds_test,
-                 ds_info,
-                 run_paths)
+    print('Main Function')
 
 if __name__ == "__main__":
+    # Importing the programs according to the sequence as mentioned in the README.md
+    print(Project_Header)
+    import input_pipeline.preprocessing
+    import models.layers
+    import evaluation.eval
     app.run(main)
