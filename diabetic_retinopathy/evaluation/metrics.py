ef626076ec258564e013903b7d0d3366eb46f5e3
st169481 <st169481@stud.uni-stuttgart.de>
2021-02-14 10:30:54 +0100

    Update metrics.py

--- diabetic_retinopathy/evaluation/metrics.py
+++ diabetic_retinopathy/evaluation/metrics.py
@@ -1,13 +1,51 @@
+'''Metrics : Confusion Matrix
+Displays the true postives and true negatives'''
+
 import tensorflow as tf
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from input_pipeline.datasets import load
+import numpy as np
+from evaluation.eval import mdl
+import matplotlib.pyplot as plt
+from input_pipeline.preprocessing import img_width,img_height
+import seaborn as sb
+import os
+import glob
+import pandas as pd
 
-class ConfusionMatrix(tf.keras.metrics.Metric):
+"""Metrics : Confusion Matrix to predict the model test results"""
 
-    def __init(self, name="confusion_matrix", **kwargs):
-        super(ConfusionMatrix, self).__init__(name=name, **kwargs)
-        # ...
+#The test images are loaded along with their ground truths
+test_images = glob.glob("/home/swetha/IDRID_dataset/images/images/test/*.jpg")
+print('Total number of training images:', len(test_images))
+df_test = pd.read_csv('/home/swetha/IDRID_dataset/labels/test.csv')
+df_test['Retinopathy grade'] = df_test['Retinopathy grade'].replace([0, 1, 2, 3, 4], [0, 0, 1, 1, 1])
+df_test['Image name'] = df_test['Image name'] + '.jpg'
+df_test = df_test.drop_duplicates()
+df_test = df_test.iloc[:, : 2]
+#Debug
+#print(df_test)
 
-    def update_state(self, *args, **kwargs):
-        # ...
+predicted_label_list = []
+for iname, iclass in df_test.itertuples(index=False):
+    for file in test_images:
+      if os.path.basename(file) == iname:
+        img = tf.io.read_file(file)
+        img = tf.io.decode_jpeg(img)
+        img = tf.cast(img,tf.float32) / 255
+        img = tf.image.resize_with_pad(img, img_height, img_width, antialias=True)
+        img = tf.reshape(img, [1, img_height, img_width, 3])
+        x = mdl.predict(img)
+        predicted_label = np.argmax(x) #The predicted label is appeneded to list after model evaluation
+        predicted_label_list.append(predicted_label)
 
-    def result(self):
-        # ...
+df_test['Predicted Class'] = predicted_label_list
+df_test['Result'] = np.where(df_test['Retinopathy grade'] == df_test['Predicted Class'], 'Correct Prediction', 'Incorrect Prediction')
+#print(df_test)
+df_test['Result'].value_counts() #The correct and incorrect labels are listed
+cm = confusion_matrix(df_test['Retinopathy grade'],df_test['Predicted Class']) #Plot the confusion matrix
+plt.figure(figsize = (10,5))
+plt.title('Confusion Matrix')
+sb.heatmap(cm, cmap="Blues", annot=True,annot_kws={"size": 16})
+print('Test Accuracy:',metrics.accuracy_score(df_test['Retinopathy grade'], df_test['Predicted Class'])) #Obtain the test accuracy
